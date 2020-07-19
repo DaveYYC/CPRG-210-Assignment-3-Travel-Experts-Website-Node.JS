4a39013e8d9d5ea9d1fa318a80f6ef2cd4c18472
DaveYYC <DaveYYC@users.noreply.github.com>
2020-07-19 14:21:23 -0600

    tweaks

--- seeds/destinations.js
+++ seeds/destinations.js
@@ -3,32 +3,46 @@ const destinations = [
     id: 1022,
     title: 'Alaska',
     description: 'picture of the northern lights',
-    paragraph: ''.
+    heading: '',
+    fact1: '',
+    fact2: '',
+    fact3: ''
   },
   {
     id: 1029, 
     title: 'New York',
     description: 'picture of central park',
-    paragraph: '', 
+    heading: '',
+    fact1: '',
+    fact2: '',
+    fact3: ''
   },
   {
     id: 1040, 
     title: 'Germany',
     description: 'picture of german castle',
-    paragraph: '',
+    heading: '',
+    fact1: '',
+    fact2: '',
+    fact3: ''
   },
   {
     id: 164, 
     title: 'Italy',
     description: 'https://italia.it/en/home.html',
-    paragraph: '',
-
+    heading: '',
+    fact1: '',
+    fact2: '',
+    fact3: ''
   },
   {
     id: 234, 
     title: 'Paris',
     description: 'https://en.parisinfo.com',
-    paragraph: '',
+    heading: '',
+    fact1: '',
+    fact2: '',
+    fact3: ''
   },
 ];
 
