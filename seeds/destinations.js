1a79187d7b5f14b01d750ed8a46706fa77c85961
DaveYYC <DaveYYC@users.noreply.github.com>
2020-07-19 13:28:06 -0600

    working on item paragraph

--- seeds/destinations.js
+++ seeds/destinations.js
@@ -3,35 +3,32 @@ const destinations = [
     id: 1022,
     title: 'Alaska',
     description: 'picture of the northern lights',
-    link: 'http://travelalaska.com/',
-    paragraph: "More info here...................."
+    paragraph: ''.
   },
   {
     id: 1029, 
     title: 'New York',
     description: 'picture of central park',
-    link: 'http://iloveny.com'
+    paragraph: '', 
   },
   {
     id: 1040, 
     title: 'Germany',
     description: 'picture of german castle',
-    link:  'https://www.germany.travel/en/index.html'
-
+    paragraph: '',
   },
   {
     id: 164, 
     title: 'Italy',
     description: 'https://italia.it/en/home.html',
-    link: 'http://www.italia.it/en/home.html'
+    paragraph: '',
 
   },
   {
     id: 234, 
     title: 'Paris',
     description: 'https://en.parisinfo.com',
-    link: 'https://en.parisinfo.com/'
-
+    paragraph: '',
   },
 ];
 
