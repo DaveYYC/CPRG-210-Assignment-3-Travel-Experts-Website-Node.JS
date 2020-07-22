162692d2a010fc6ba61552f851dc7348ee3af982
DaveYYC <DaveYYC@users.noreply.github.com>
2020-07-22 14:55:44 -0600

    tweaks

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.get('/', function(request, response){
 
 // Endpoint handler for the login page //
 app.get('/login', function(request, response){
-  response.render('login',{});
+  response.render('login',{title:'login'});
 });
 
 // Endpoint handler for the register page //
